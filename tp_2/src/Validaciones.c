60d9ac11ab52dcf78b73924937ad697318e81e0c
nicolascaliari <nicolascaliari28@gmail.com>
2022-10-25 10:49:53 -0300

    tp2

---
+++ tp_2/src/Validaciones.c
@@ -0,0 +1,8 @@
+/*
+ * Validaciones.c
+ *
+ *  Created on: 25 oct. 2022
+ *      Author: nicolas
+ */
+
+
---
+++ tp_2/src/Validaciones.h
@@ -0,0 +1,13 @@
+/*
+ * Validaciones.h
+ *
+ *  Created on: 25 oct. 2022
+ *      Author: nicolas
+ */
+
+#ifndef VALIDACIONES_H_
+#define VALIDACIONES_H_
+
+
+
+#endif /* VALIDACIONES_H_ */
